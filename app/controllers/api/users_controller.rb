d9ad7f00e2167afa8b484137cd7a8d8cd35d1464
Jeremy <thejeremyjohn@gmail.com>
2018-03-06 15:29:18 -0500

    can login with ajax, beginning session_api_util

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,22 @@
 class Api::UsersController < ApplicationController
+  def new
+  end
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: {errors: @user.errors.full_messages}, status: 422
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 
   def show
   end
 
-  def new
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'users/create'
-  end
-
-  namespace :api do
-    get 'users/show'
-  end
-
-  namespace :api do
-    get 'users/new'
-  end
-
-  get 'static_pages/root'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:new, :create, :show]
+    resource :session, only: [:new, :create, :destroy]
+  end
+
+
+  root 'static_pages#root'
 end
